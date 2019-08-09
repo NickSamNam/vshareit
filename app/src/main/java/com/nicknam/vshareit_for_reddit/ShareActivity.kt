163bb702d34ea5a36bc4e54de8c630a347521f9d
Sam Nicknam <s.nicknam@gmail.com>
2019-08-09 12:00:09 +0200

    Tiny warning fix

--- app/src/main/java/com/nicknam/vshareit_for_reddit/ShareActivity.kt
+++ app/src/main/java/com/nicknam/vshareit_for_reddit/ShareActivity.kt
@@ -10,7 +10,7 @@ import android.widget.Toast
 
 class ShareActivity : AppCompatActivity() {
 
-    private var url: String? = null
+    private var url: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
