c813c583bf0dc69c283f3935f9c8a23285f730a9
Sam Nicknam <s.nicknam@gmail.com>
2019-08-12 15:47:19 +0200

    Added keyboard go action to url field

--- app/src/main/java/com/nicknam/vshareit_for_reddit/MainActivity.kt
+++ app/src/main/java/com/nicknam/vshareit_for_reddit/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.nicknam.vshareit_for_reddit
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.nicknam.vshareit_for_reddit.util.share
 
@@ -19,10 +22,22 @@ class MainActivity : AppCompatActivity() {
         etUrl = findViewById(R.id.et_url)
 
         btnShare.setOnClickListener(this::onShareClick)
+        etUrl.setOnEditorActionListener(this::onUrlEditorAction)
     }
 
     @Suppress("UNUSED_PARAMETER")
     private fun onShareClick(v: View) {
         share(this, etUrl.text.toString())
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    private fun onUrlEditorAction(v: TextView, actionId: Int, event: KeyEvent?): Boolean {
+        return when (actionId) {
+            EditorInfo.IME_ACTION_GO -> {
+                share(this, etUrl.text.toString())
+                true
+            }
+            else -> false
+        }
     }
 }
